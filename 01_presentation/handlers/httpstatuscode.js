5524540c1dab4e193d411184ab2d4a751fb5230e
Allan Wanderley <awallan259@gmail.com>
2020-08-02 01:04:38 -0300

    Fix: FIxed unexpected errors response

--- 01_presentation/handlers/httpstatuscode.js
+++ 01_presentation/handlers/httpstatuscode.js
@@ -1,10 +1,17 @@
+const Logger = require('../../03_infra/services/log')
+
 class HttpStatusCodeHandler {
   static Configure(express) {
     express.use(HttpStatusCodeHandler.httpStatusCodeErrorHandler)
   }
 
   static httpStatusCodeErrorHandler(err, req, res, next) {
-    res.status(err.statusCode).json(err)
+    if (err.statusCode) {
+      res.status(err.statusCode).json(err)
+    } else {
+      Logger.print(err.stack || err)
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
   }
 }
 
