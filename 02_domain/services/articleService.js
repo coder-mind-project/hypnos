7ad5bd8f33f04f41dcd34e33f4b22213b2ecddaa
Allan Wanderley <awallan259@gmail.com>
2020-08-02 00:34:56 -0300

    #3 - Finished partial articles refactoring

--- 02_domain/services/articleService.js
+++ 02_domain/services/articleService.js
@@ -1,19 +1,15 @@
 const ServiceLocator = require('../../03_infra/dependencyInjection/serviceLocator')
 
 class ArticleService {
-  async getBoostedArticles(skip = 0, take = 5) {
+  getBoostedArticles(skip = 0, take = 5) {
     return ServiceLocator.unitOfWork.articleRepository.getBoosted(skip, take)
   }
 
-  async getByCustomUri(customUri) {
+  getByCustomUri(customUri) {
     return ServiceLocator.unitOfWork.articleRepository.getByCustomUri(customUri, ['boosted', 'published'])
   }
 
-  async getPopularArticles(skip = 0, take = 10) {
-    return ServiceLocator.unitOfWork.articleRepository.getPopulars(skip, take)
-  }
-
-  async getRelateds(articleUri, limit = 5) {
+  getRelateds(articleUri, limit = 5) {
     return ServiceLocator.unitOfWork.articleRepository.getRelateds(articleUri, limit)
   }
 }
