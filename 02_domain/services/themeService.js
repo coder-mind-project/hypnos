b3cd2974e850a04832194160a2dc8a90f0436460
Allan Wanderley <awallan259@gmail.com>
2020-08-02 01:05:20 -0300

    Refactor: Changed class name

--- 02_domain/services/themeService.js
+++ 02_domain/services/themeService.js
@@ -1,9 +1,9 @@
 const ServiceLocator = require('../../03_infra/dependencyInjection/serviceLocator')
 
-class ArticleService {
+class ThemeService {
   get(skip = 0, take = 5) {
     return ServiceLocator.unitOfWork.themeRepository.get(skip, take, { state: 'active' })
   }
 }
 
-module.exports = new ArticleService()
+module.exports = new ThemeService()
