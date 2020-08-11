7f3c8d2632789afffc5c4361317736c265f257de
Allan Wanderley <awallan259@gmail.com>
2020-08-11 06:12:23 -0300

    Chore: Removed stack error

--- src/01_presentation/exceptions/InvalidArgument.ts
+++ src/01_presentation/exceptions/InvalidArgument.ts
@@ -2,13 +2,11 @@ import IErrorException from './IErrorException'
 
 class InvalidArgument extends Error implements IErrorException {
   public statusCode: number
-  public stackError?: string
 
   constructor(msg: string) {
     super()
     this.message = msg
     this.statusCode = 400
-    this.stackError = this.stack
   }
 }
 
