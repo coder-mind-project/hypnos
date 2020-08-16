42f511f12dfd978fdcd04aad4fe588d7f711893b
Allan Wanderley <awallan259@gmail.com>
2020-08-15 21:10:07 -0300

    #14 - Fix: Fixed cross validation

---
+++ src/01_presentation/exceptions/NotAuthorized.ts
@@ -0,0 +1,13 @@
+import IErrorException from './IErrorException'
+
+class NotAuthorized extends Error implements IErrorException {
+    public statusCode: number
+
+    constructor(msg: string) {
+        super()
+        this.message = msg
+        this.statusCode = 401
+    }
+}
+
+export default NotAuthorized
