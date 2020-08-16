1078159d481f2528877869bb45a55e9c854057f7
Allan Wanderley <awallan259@gmail.com>
2020-08-15 21:52:02 -0300

    #23 - Refactored likes section

---
+++ src/03_infra/database/repositories/likeRepository.ts
@@ -0,0 +1,11 @@
+import BaseRepository from './baseRepository'
+import Like from '../../../02_domain/entities/Like'
+import ILikeRepository from '../../interfaces/repositories/ILikeRepository'
+
+class LikeRepository extends BaseRepository implements ILikeRepository {
+    constructor() {
+        super(Like)
+    }
+}
+
+export default LikeRepository
--- src/03_infra/interfaces/IUnitOfWork.ts
+++ src/03_infra/interfaces/IUnitOfWork.ts
@@ -2,12 +2,14 @@ import IArticleRepository from "./repositories/IArticleRepository";
 import ICommentRepository from "./repositories/ICommentRepository";
 import IThemeRepository from "./repositories/IThemeRepository";
 import IViewRepository from "./repositories/IViewRepository";
+import ILikeRepository from "./repositories/ILikeRepository";
 
 interface IUnitOfWork {
     articleRepository: IArticleRepository
     themeRepository: IThemeRepository
     commentRepository: ICommentRepository
     viewRepository: IViewRepository
+    likeRepository: ILikeRepository
 }
 
 export default IUnitOfWork
